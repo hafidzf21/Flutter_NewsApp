77aab1edfcfd1c74fd9cf8f22a384aac9bc1ad97
Hafidz <hafidzfadhillah606@gmail.com>
2021-08-09 12:47:48 +0700

    Http Request Delete

--- lib/pages/menu/student_pages.dart
+++ lib/pages/menu/student_pages.dart
@@ -10,66 +10,76 @@ class StudentPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final allStudentsProvider = Provider.of<Students>(context);
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("All Students"),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.person_add),
-            onPressed: () {
-              Navigator.pushNamed(context, AddStudent.routeName);
-            },
-          ),
-        ],
-      ),
-      body: (allStudentsProvider.jumlahStudent == 0) ? Container(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "No Data",
-              style: TextStyle(fontSize: 25),
-            ),
-            SizedBox(height: 20),
-            OutlinedButton(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("All Students"),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.person_add),
               onPressed: () {
                 Navigator.pushNamed(context, AddStudent.routeName);
-              }, 
-              child: Text(
-                "Add Student",
-                style: TextStyle(fontSize: 20),
-              )
+              },
             ),
           ],
         ),
-      )
-      : ListView.builder(
-        itemCount: allStudentsProvider.jumlahStudent,
-        itemBuilder: (context, index) {
-          var id = allStudentsProvider.allStudent[index].id;
-          return ListTile(
-            onTap: () {
-              Navigator.pushNamed(context, DetailStudent.routeName, arguments: id);
-            },
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(allStudentsProvider.allStudent[index].imageUrl),
-            ),
-            title: Text(
-              allStudentsProvider.allStudent[index].name,
-            ),
-            subtitle: Text(
-              allStudentsProvider.allStudent[index].position,
-            ),
-            trailing: IconButton(
-              onPressed: () {
-                allStudentsProvider.deleteStudent(id, context);
-              },
-              icon: Icon(Icons.delete),
-            ),
-          );
-        },
-      )
-    );
+        body: (allStudentsProvider.jumlahStudent == 0)
+            ? Container(
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "No Data",
+                      style: TextStyle(fontSize: 25),
+                    ),
+                    SizedBox(height: 20),
+                    OutlinedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, AddStudent.routeName);
+                        },
+                        child: Text(
+                          "Add Student",
+                          style: TextStyle(fontSize: 20),
+                        )),
+                  ],
+                ),
+              )
+            : ListView.builder(
+                itemCount: allStudentsProvider.jumlahStudent,
+                itemBuilder: (context, index) {
+                  var id = allStudentsProvider.allStudent[index].id;
+                  return ListTile(
+                    onTap: () {
+                      Navigator.pushNamed(context, DetailStudent.routeName,
+                          arguments: id);
+                    },
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          allStudentsProvider.allStudent[index].imageUrl),
+                    ),
+                    title: Text(
+                      allStudentsProvider.allStudent[index].name,
+                    ),
+                    subtitle: Text(
+                      allStudentsProvider.allStudent[index].position,
+                    ),
+                    trailing: IconButton(
+                      onPressed: () {
+                        allStudentsProvider.deleteStudent(id).then(
+                          (_) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text("Berhasil Dihapus!"),
+                                duration: Duration(milliseconds: 500),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                      icon: Icon(Icons.delete),
+                    ),
+                  );
+                },
+              ));
   }
 }
--- lib/providers/student.dart
+++ lib/providers/student.dart
@@ -14,8 +14,7 @@ class Students with ChangeNotifier {
   Student selectById(String id) =>
       _allStudent.firstWhere((element) => element.id == id);
 
-  Future <void> addStudent(
-      String name, String position, String image) {
+  Future<void> addStudent(String name, String position, String image) {
     DateTime dateTimeNow = DateTime.now();
 
     Uri url = Uri.parse(
@@ -32,7 +31,7 @@ class Students with ChangeNotifier {
         },
       ),
     )
-      .then(
+        .then(
       (response) {
         _allStudent.add(
           Student(
@@ -59,9 +58,15 @@ class Students with ChangeNotifier {
     notifyListeners();
   }
 
-  void deleteStudent(String id, BuildContext context) {
-    _allStudent.removeWhere((element) => element.id == id);
+  Future<void> deleteStudent(String id) {
+    Uri url = Uri.parse(
+        "https://http-req-flutter-default-rtdb.firebaseio.com/students/$id.json");
 
-    notifyListeners();
+    return http.delete(url).then(
+      (response) {
+        _allStudent.removeWhere((element) => element.id == id);
+        notifyListeners();
+      },
+    );
   }
 }
